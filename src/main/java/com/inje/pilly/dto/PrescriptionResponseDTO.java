3ff099854d6e2d8527da12954fa505f88e1780e8
hui-jeong <huijeong1028@gmail.com>
2025-04-02 00:20:56 +0900

    feat: 생성자 추가

--- src/main/java/com/inje/pilly/dto/PrescriptionResponseDTO.java
+++ src/main/java/com/inje/pilly/dto/PrescriptionResponseDTO.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class PrescriptionResponseDTO {
     private Long prescriptionId;
     private String message;
+
+    public PrescriptionResponseDTO(long prescriptionId, String 처방전_저장_완료, List<String> feedbackMessages) {
+    }
 }
