af14ac16f5c020843d54435660c27d0bb9c24078
hui-jeong <huijeong1028@gmail.com>
2025-04-07 00:36:08 +0900

    refactory: 응답 리스트 수정

--- src/main/java/com/inje/pilly/dto/SideEffectTrainRecordDTO.java
+++ src/main/java/com/inje/pilly/dto/SideEffectTrainRecordDTO.java
@@ -1,27 +1,23 @@
 package com.inje.pilly.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class SideEffectTrainRecordDTO {
+
     private Long userId;
     private Long medicineId;
     private int fatigueLevel;
     private int dizzinessLevel;
-    private int moodEncoded;
+    private String mood;
     private float sleepHours;
-    private boolean headache;
-    private boolean stomachache;
-    private boolean rash;
-    private boolean vomiting;
-    private boolean itchiness;
-
-    public SideEffectTrainRecordDTO(Long userId, Long medicineId, int fatigueLevel, int dizzinessLevel, String mood, float sleepHours, boolean sideEffectOccurred, List<String> sideEffects) {
-    }
+    private boolean sideEffectOccurred;
+    private List<String> sideEffects;
+    private String recordDate;
 }
