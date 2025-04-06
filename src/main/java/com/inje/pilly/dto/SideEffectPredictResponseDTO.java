fcaaf97ccac0f91b6a631cc3a4f9ccea79cfe1ce
hui-jeong <huijeong1028@gmail.com>
2025-04-07 00:35:41 +0900

    refactory: 약 이름 조합 추가

--- src/main/java/com/inje/pilly/dto/SideEffectPredictResponseDTO.java
+++ src/main/java/com/inje/pilly/dto/SideEffectPredictResponseDTO.java
@@ -1,15 +1,17 @@
 package com.inje.pilly.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class SideEffectPredictResponseDTO {
+    private Long medicineId;
+    private String medicineName;
     private Map<String, Double> probabilities;
     private String feedback;
 }
